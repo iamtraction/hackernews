2bc38f9eee4c628546da916bec42bd0a079596ce
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-09-07 08:07:04 +0530

    components: add StoryType component
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

---
+++ components/StoryType.jsx
@@ -0,0 +1,23 @@
+const StoryType = ({ name, Icon, handler, selected }) => (
+    <div
+        style={{
+            padding: "5px 10px",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            gap: 5,
+            backgroundColor: selected === name.toLowerCase() ? "rgba(100, 100, 100, .5)" : "transparent",
+            border: "1px solid",
+            borderColor: selected === name.toLowerCase() ? "rgba(100, 100, 100, .5)" : "transparent",
+            color: selected === name.toLowerCase() ? "white" : "var(--text)",
+            borderRadius: 20,
+            cursor: "pointer",
+        }}
+        onClick={ () => handler(name.toLowerCase()) }
+    >
+        <Icon />
+        <span>{ name }</span>
+    </div>
+);
+
+export default StoryType;
