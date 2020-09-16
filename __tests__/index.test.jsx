df44b7940d0732a00d01fdcd614042fb565055a9
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-09-16 10:06:54 +0530

    tests: move page tests to __tests__ dir
    it prevents Vercel to read tests as pages
    
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

--- __tests__/index.test.jsx
+++ __tests__/index.test.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import Home from "./";
+import Home from "../pages";
 
 it("Home page renders without crashing", () => {
     ReactDOM.render(
